f06714b539f4e061e7c5450433d64324d2c2ce40
fuoco <recyger@gmail.com>
2016-04-01 15:57:49 +0300

    init

--- Plugin.php
+++ Plugin.php
@@ -5,12 +5,12 @@ namespace testing\composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+
 class Plugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
-        }
     }
 }
