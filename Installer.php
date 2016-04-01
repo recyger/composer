927faaed966191dcd711d3aaaee5391b9f46a292
fuoco <recyger@gmail.com>
2016-04-01 17:16:23 +0300

    init

--- Installer.php
+++ Installer.php
@@ -2,11 +2,28 @@
 
 namespace testing\composer;
 
-use Composer\Package\PackageInterface;
+use Composer\Composer;
+use Composer\Installer\BinaryInstaller;
+use Composer\IO\IOInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\Util\Filesystem;
+
 class Installer extends LibraryInstaller
 {
     const TYPE_NAME = 'test';
+
+
+    public function __construct(
+        IOInterface $io,
+        Composer $composer,
+        $type = 'library',
+        Filesystem $filesystem = null,
+        BinaryInstaller $binaryInstaller = null
+    )
+    {
+        parent::__construct($io, $composer, $type, $filesystem, $binaryInstaller);
+        $this->vendorDir .= '/../';
+    }
 
 
     /**
@@ -15,9 +32,5 @@ class Installer extends LibraryInstaller
     public function supports($packageType)
     {
         return strncasecmp($packageType, self::TYPE_NAME, strlen(self::TYPE_NAME)) === 0;
-    }
-    
-    public function getInstallPath(PackageInterface $package) {
-        return $this->vendorDir . '/../testing/';
     }
 }
